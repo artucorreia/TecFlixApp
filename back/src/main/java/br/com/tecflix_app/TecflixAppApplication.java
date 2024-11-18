20c6bbe80e358b099121fa4cfdedebbed03b025d
artucorreia <arthurcorria0109@gmail.com>
2024-11-18 16:46:46 -0300

    :tada: First commit

---
+++ back/src/main/java/br/com/tecflix_app/TecflixAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecflix_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecflixAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecflixAppApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/br/com/tecflix_app/TecflixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecflix_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecflixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
