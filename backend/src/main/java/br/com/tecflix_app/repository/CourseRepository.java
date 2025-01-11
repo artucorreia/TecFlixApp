08bf2afc26e9aadf0be3e4d338e8222df0bc552b
artucorreia <arthurcorria0109@gmail.com>
2025-01-11 16:02:04 -0300

    fix(CourseRepository.java): fix select the same course more than once by tags

--- backend/src/main/java/br/com/tecflix_app/repository/CourseRepository.java
+++ backend/src/main/java/br/com/tecflix_app/repository/CourseRepository.java
@@ -17,88 +17,78 @@ import br.com.tecflix_app.projection.CourseProjection;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, UUID> {
     Optional<CourseDetailsProjection> findDetailsById(UUID id);
-    
+
     Page<CourseProjection> findAllBy(Pageable pageable);
 
-    @Query(
-        """
-            SELECT 
-                c.id as id,
-                c.title as title,
-                c.capeImage as capeImage,
-                c.createdAt as createdAt,
-                c.totalScoreReviews as totalScoreReviews,
-                c.totalReviews as totalReviews,
-                p as professor
-            FROM 
-                Course c
-            JOIN 
-                c.tags t
-            JOIN
-                c.professor p
-            WHERE 
-                t.id IN :tagIds
-        """
-    )
+    @Query("""
+                SELECT DISTINCT
+                    c.id as id,
+                    c.title as title,
+                    c.capeImage as capeImage,
+                    c.createdAt as createdAt,
+                    c.totalScoreReviews as totalScoreReviews,
+                    c.totalReviews as totalReviews,
+                    p as professor
+                FROM
+                    Course c
+                JOIN
+                    c.tags t
+                JOIN
+                    c.professor p
+                WHERE
+                    t.id IN :tagIds
+            """)
     Page<CourseProjection> findByTagIds(Long[] tagIds, Pageable pageable);
 
-    @Query(
-        """
-            SELECT
-                c
-            FROM
-                Course c
-            WHERE
-                UPPER(CONCAT(c.title, c.description)) 
-            LIKE 
-                UPPER(CONCAT('%', :term, '%'))
-        """
-    )
+    @Query("""
+                SELECT
+                    c
+                FROM
+                    Course c
+                WHERE
+                    UPPER(CONCAT(c.title, c.description))
+                LIKE
+                    UPPER(CONCAT('%', :term, '%'))
+            """)
     Page<CourseProjection> findByTerm(String term, Pageable pageable);
 
-
-    @Query(
-        """
-            SELECT 
-                c
-            FROM 
-                Course c
-            JOIN 
-                c.tags t
-            WHERE 
-                t.id IN :tagIds
-            AND 
-                UPPER(CONCAT(c.title, c.description)) 
-            LIKE 
-                UPPER(CONCAT('%', :term, '%'))
-        """
-    )
+    @Query("""
+                SELECT
+                    c
+                FROM
+                    Course c
+                JOIN
+                    c.tags t
+                WHERE
+                    t.id IN :tagIds
+                AND
+                    UPPER(CONCAT(c.title, c.description))
+                LIKE
+                    UPPER(CONCAT('%', :term, '%'))
+            """)
     Page<CourseProjection> findByTagIdsAndTerm(Long[] tagIds, String term, Pageable pageable);
 
     @Modifying
-    @Query(
-        nativeQuery = true,
-        value = """
-                    UPDATE 
-                        courses 
-                    SET 
-                        total_reviews = (
-                            SELECT 
-                                COUNT(r.id) 
-                            FROM 
-                                reviews r 
-                            WHERE 
-                                r.course_id = courses.id
-                        ), 
-                        total_score_reviews = (
-                            SELECT 
-                                SUM(r.score) 
-                            FROM 
-                                reviews r 
-                            WHERE 
-                                r.course_id = courses.id
-                        );
-                """
-    )
+    @Query(nativeQuery = true, value = """
+                UPDATE
+                    courses
+                SET
+                    total_reviews = (
+                        SELECT
+                            COUNT(r.id)
+                        FROM
+                            reviews r
+                        WHERE
+                            r.course_id = courses.id
+                    ),
+                    total_score_reviews = (
+                        SELECT
+                            SUM(r.score)
+                        FROM
+                            reviews r
+                        WHERE
+                            r.course_id = courses.id
+                    );
+            """)
     void updateCourseReviews();
 }
