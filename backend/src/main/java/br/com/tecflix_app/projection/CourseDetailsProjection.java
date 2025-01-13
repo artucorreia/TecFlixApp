4fdb8244b65b8b5ce0d17523a308452540c927d1
artucorreia <arthurcorria0109@gmail.com>
2025-01-13 15:21:40 -0300

    feat(CourseDetailsProjection.java): add classes to response

--- backend/src/main/java/br/com/tecflix_app/projection/CourseDetailsProjection.java
+++ backend/src/main/java/br/com/tecflix_app/projection/CourseDetailsProjection.java
@@ -6,24 +6,42 @@ import java.util.UUID;
 
 public interface CourseDetailsProjection {
     UUID getId();
+
     String getTitle();
+
     String getDescription();
+
     String getCapeImage();
+
     Boolean getActive();
+
     LocalDateTime getCreatedAt();
+
     Double getTotalScoreReviews();
+
     Long getTotalReviews();
+
     UserBasicProjection getProfessor();
+
     List<ModuleCourseDetailsProjection> getModules();
+
     List<TagCourseDetailsProjection> getTags();
 
     public interface TagCourseDetailsProjection {
         Long getId();
+
         String getName();
     }
 
     public interface ModuleCourseDetailsProjection {
         Long getId();
+
+        String getTitle();
+
+        List<ClassCourseDetailsProjection> getClasses();
+    }
+
+    public interface ClassCourseDetailsProjection {
         String getTitle();
     }
 }
