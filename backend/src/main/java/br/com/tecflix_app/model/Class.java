1c9c220cf7ae0cfd60466cc50edb44289d0e2c4a
artucorreia <arthurcorria0109@gmail.com>
2025-01-13 15:20:56 -0300

    fix(Class.java): fix attribute name

--- backend/src/main/java/br/com/tecflix_app/model/Class.java
+++ backend/src/main/java/br/com/tecflix_app/model/Class.java
@@ -22,25 +22,27 @@ import lombok.Setter;
 @Table(name = "classes")
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter @Setter
+@Getter
+@Setter
 @EqualsAndHashCode(of = "id")
 public class Class implements Serializable {
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false, length = 20)
     private String title;
-    
+
     @Column(nullable = false, name = "video_path")
     private String videoPath;
-    
+
     @Column(nullable = false)
     private Boolean active;
 
     @Column(nullable = false, name = "created_at", updatable = false)
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
-    @ManyToOne 
+    @ManyToOne
     @JoinColumn(name = "module_id", referencedColumnName = "id", nullable = false)
     private Module module;
 }
