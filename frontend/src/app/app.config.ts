b69bb90a3462b1c24f98ce5b17586a09afd072c9
artucorreia <arthurcorria0109@gmail.com>
2025-01-25 15:16:08 -0300

    feat: add angular/fire

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,20 +5,34 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ 
-      eventCoalescing: true 
-    }), 
+    provideZoneChangeDetection({
+      eventCoalescing: true,
+    }),
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
-      }
-    })
-  ]
+        preset: Aura,
+      },
+    }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'tecflix-academy',
+        appId: '1:378803860813:web:dd6e0b4946351959c23304',
+        storageBucket: 'tecflix-academy.firebasestorage.app',
+        apiKey: 'AIzaSyDo9XoMs12ydULY6fpKduukz8XQxi2I-ZM',
+        authDomain: 'tecflix-academy.firebaseapp.com',
+        messagingSenderId: '378803860813',
+        measurementId: 'G-CD2BW4QN33',
+      }),
+    ),
+    provideStorage(() => getStorage()),
+  ],
 };
