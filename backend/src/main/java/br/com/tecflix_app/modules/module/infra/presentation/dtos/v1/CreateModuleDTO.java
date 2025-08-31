e492dcbd0ac29ff2168be48985250bccdec7dadf
Arthur Correia <arthurcorria0109@gmail.com>
2025-08-30 22:55:07 -0300

    refactor: remove course object and change to courseId parameter

--- backend/src/main/java/br/com/tecflix_app/modules/module/infra/presentation/dtos/v1/CreateModuleDTO.java
+++ backend/src/main/java/br/com/tecflix_app/modules/module/infra/presentation/dtos/v1/CreateModuleDTO.java
@@ -1,10 +1,5 @@
 package br.com.tecflix_app.modules.module.infra.presentation.dtos.v1;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import br.com.tecflix_app.modules.course.infra.presentation.dtos.v1.CourseResponseDTO;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,19 +8,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class CreateModuleDTO {
-  @NotNull
-  @NotBlank
-  @Size(min = 5, max = 40)
+  @NotNull(message = "O título do módulo é obrigatório")
+  @NotBlank(message = "O título do módulo é obrigatório")
+  @Size(min = 5, max = 40, message = "O título do módulo deve ter entre 5 e 40 caracteres")
   private String title;
 
-  @NotNull private CourseResponseDTO course;
-
-  @JsonIgnore private Boolean active = true;
-
-  @JsonIgnore private LocalDateTime createdAt = LocalDateTime.now();
+  @NotNull(message = "O id do curso é obrigatório")
+  private UUID courseId;
 }
--- backend/src/main/java/br/com/tecflix_app/modules/module/infra/presentation/mapper/ModulePresentationMapper.java
+++ backend/src/main/java/br/com/tecflix_app/modules/module/infra/presentation/mapper/ModulePresentationMapper.java
@@ -1,8 +1,11 @@
 package br.com.tecflix_app.modules.module.infra.presentation.mapper;
 
 import br.com.tecflix_app.modules.module.application.domain.entity.Module;
+import br.com.tecflix_app.modules.module.infra.presentation.dtos.v1.CreateModuleDTO;
 import br.com.tecflix_app.modules.module.infra.presentation.dtos.v1.ModuleResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
@@ -11,4 +14,13 @@ public interface ModulePresentationMapper {
   ModuleResponseDTO domainToResponseDTO(Module module);
 
   List<ModuleResponseDTO> domainToResponseDTO(List<Module> modules);
+
+  @Mappings(
+      value = {
+        @Mapping(target = "id", ignore = true),
+        @Mapping(target = "active", ignore = true),
+        @Mapping(target = "createdAt", ignore = true),
+        @Mapping(target = "course.id", source = "courseId")
+      })
+  Module createDTOToDomain(CreateModuleDTO createModuleDTO);
 }
