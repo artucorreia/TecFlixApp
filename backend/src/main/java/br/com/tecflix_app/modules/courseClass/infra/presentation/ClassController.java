8bd4e8b00502c3ff3923570016b1a11e78e3b434
Arthur Correia <arthurcorria0109@gmail.com>
2025-09-04 22:30:04 -0300

    cleanup(ClassController): remove unnecessary endpoints

--- backend/src/main/java/br/com/tecflix_app/modules/courseClass/infra/presentation/ClassController.java
+++ backend/src/main/java/br/com/tecflix_app/modules/courseClass/infra/presentation/ClassController.java
@@ -1,8 +1,5 @@
 package br.com.tecflix_app.modules.courseClass.infra.presentation;
 
-import java.util.List;
-import java.util.UUID;
-
 import br.com.tecflix_app.modules.courseClass.application.domain.entity.Class;
 import br.com.tecflix_app.modules.courseClass.application.usecases.CreateClassUseCase;
 import br.com.tecflix_app.modules.courseClass.constant.ClassConstant;
@@ -11,23 +8,17 @@ import br.com.tecflix_app.modules.shared.dto.v1.ResponseDTO;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tecflix_app.modules.courseClass.infra.presentation.dtos.v1.CreateClassDTO;
-import br.com.tecflix_app.modules.courseClass.infra.presentation.dtos.v1.ClassDTO;
 import br.com.tecflix_app.modules.shared.dto.v1.GenericResponseDTO;
-import br.com.tecflix_app.service.ClassService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -44,58 +35,8 @@ import jakarta.validation.Valid;
 @RequiredArgsConstructor
 public class ClassController {
 
-  private final ClassService service;
   private final CreateClassUseCase createClassUseCase;
   private final ClassPresentationMapper classPresentationMapper;
-
-  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  @Operation(
-      summary = "Find class by id",
-      description = "Find class by id",
-      tags = {"Class"},
-      method = "GET")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Success",
-            content =
-                @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = ClassDTO.class))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
-      })
-  public ResponseEntity<ClassDTO> findById(@PathVariable UUID id) {
-    return ResponseEntity.ok(service.findById(id));
-  }
-
-  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  @Operation(
-      summary = "Find all classes",
-      description = "Find all classes",
-      tags = {"Class"},
-      method = "GET")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Success",
-            content =
-                @Content(
-                    mediaType = "application/json",
-                    array = @ArraySchema(schema = @Schema(implementation = ClassDTO.class)))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
-      })
-  public ResponseEntity<List<ClassDTO>> findAll() {
-    return ResponseEntity.ok(service.findAll());
-  }
 
   @PostMapping(
       consumes = MediaType.APPLICATION_JSON_VALUE,
