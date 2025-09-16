742d4669e274b2411135eeaf1e1f47238b20ba2c
Arthur Correia <arthurcorria0109@gmail.com>
2025-09-16 11:28:46 -0300

    cleanup(TokenResponseDTO): remove userId field

--- backend/src/main/java/br/com/tecflix_app/modules/auth/infra/presentation/dtos/v1/TokenResponseDTO.java
+++ backend/src/main/java/br/com/tecflix_app/modules/auth/infra/presentation/dtos/v1/TokenResponseDTO.java
@@ -1,7 +1,6 @@
 package br.com.tecflix_app.modules.auth.infra.presentation.dtos.v1;
 
 import java.time.Instant;
-import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +14,6 @@ import lombok.Setter;
 @Setter
 @Builder
 public class TokenResponseDTO {
-  private UUID userId;
   private String accessToken;
   private String refreshToken;
   private Instant issuedAt;
