dc3043b5f86337ff2a1e85826a2791642def8e8d
Arthur Correia <arthurcorria0109@gmail.com>
2025-09-26 19:34:59 -0300

    fix: fix foreign key column names

--- backend/src/main/resources/db/migration/structure/V7__create_table_professors_data.sql
+++ backend/src/main/resources/db/migration/structure/V7__create_table_professors_data.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS professors_data(
     id SERIAL PRIMARY KEY,
     user_id UUID REFERENCES users(id) UNIQUE NOT NULL,
-    occupation SERIAL REFERENCES occupations(id) NOT NULL,
+    occupation_id SERIAL REFERENCES occupations(id) NOT NULL,
     other_occupation VARCHAR(50),
     biography TEXT NOT NULL,
     birthdate DATE NOT NULL,
-    gender SERIAL REFERENCES genders(id) NOT NULL,
+    gender_id SERIAL REFERENCES genders(id) NOT NULL,
     other_gender VARCHAR(30),
     phone_number VARCHAR(13) UNIQUE NOT NULL,
     profile_image_url VARCHAR(255),
