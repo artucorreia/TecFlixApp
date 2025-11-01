e56cc2e454a3c4d4e06556e0c37129aa12740bfe
Arthur Correia <arthurcorria0109@gmail.com>
2025-11-01 19:54:19 -0300

    docs(AuthenticatedUserGatewayImpl): add logs

--- backend/src/main/java/br/com/tecflix_app/modules/auth/infra/gateways/AuthenticatedUserGatewayImpl.java
+++ backend/src/main/java/br/com/tecflix_app/modules/auth/infra/gateways/AuthenticatedUserGatewayImpl.java
@@ -11,15 +11,17 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 @Component
 @RequiredArgsConstructor
 public class AuthenticatedUserGatewayImpl implements AuthenticatedUserGateway {
-
+  private final Logger LOGGER = Logger.getLogger(AuthenticatedUserGatewayImpl.class.getName());
   private final AuthGatewaysMapper authGatewaysMapper;
 
   @Override
   public Optional<UUID> findId() {
+    LOGGER.info("Find authenticated user's id");
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if (authentication != null && authentication.isAuthenticated()) {
       UserEntity principal = (UserEntity) authentication.getPrincipal();
@@ -30,6 +32,7 @@ public class AuthenticatedUserGatewayImpl implements AuthenticatedUserGateway {
 
   @Override
   public Optional<User> findUser() {
+    LOGGER.info("Find authenticated user");
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if (authentication != null && authentication.isAuthenticated()) {
       UserEntity principal = (UserEntity) authentication.getPrincipal();
